85b1a4b263f1778cf1ae55abf64df0cf5aac0b36
BrandonRoyalUCF <brandonroyal@knights.ucf.edu>
2017-10-19 20:09:57 -0400

    testing node to python
    testing

--- AME/python/imgProc/Match.py
+++ AME/python/imgProc/Match.py
@@ -1,14 +1,20 @@
+#GitHub Code
+
 from StartRecognition import *
 import sys
 import cv2
 import json
+ 
 
-jsonString = sys.argv[1]
 #start = StartRecognition(jsonObject)
 #finalJson = start.startRecognition()
 
+jsonString = sys.argv[1]
 jsonObject = json.loads(jsonString)
+meetingPicBuffer = jsonObject['meetingPic']
+cv2.imshow('image', meetingPicBuffer)
+finalJson = json.dumps(meetingPicBuffer)
+sys.stdout.write(finalJson)
 
-meetingPic = jsonObject['meetingPic']
-
-sys.stdout.write(jsonString)
+#start = StartRecognition(jsonObject)
+#finalJson = start.startRecognition()
