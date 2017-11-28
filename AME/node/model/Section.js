9b374b41b470b83609098acf946e135e7daf89c5
ulmers <stephenulmer@knights.ucf.edu>
2017-11-28 17:01:32 -0500

    Section Social Data

--- AME/node/model/Section.js
+++ AME/node/model/Section.js
@@ -6,5 +6,7 @@ module.exports = mongoose.model('Section', {
     sectionID: String,//Ex: BSC2010-0001
     courseName: String,
     students: [{name: String, studentID: String, _id: String}],//"lastName, firstName" , _id
+    socialData: [{student_id: String, 
+                  relationships: [{student_id: String, value: Number}]}],
     meetings: [{dateTime: String, _id: String}]
 });
