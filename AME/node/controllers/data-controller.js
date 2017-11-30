6849df9eae559ffb403a4323e02ce2745d7a5e2a
ulmers <stephenulmer@knights.ucf.edu>
2017-11-29 21:01:21 -0500

    print

--- AME/node/controllers/data-controller.js
+++ AME/node/controllers/data-controller.js
@@ -257,6 +257,8 @@ module.exports.postMeeting= function (req, res) {
         
         meetingJSONString = "{\"meeting_id\" : \"" + meeting._id.toString() + "\" , \"section_id\" : \""+ section_id+"\"}";
         
+        console.log("json: " + meetingJSONString);
+        
         const process = exec('C:/Users/Administrator/AppData/Local/Programs/Python/Python36/python C:/AME/AME/python/MainEntry/MainEntry.py ' + meetingJSONString, function (err, stdout, stderr){
             if (err){
                 console.log(err)
